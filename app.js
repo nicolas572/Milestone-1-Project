0fcc41bbad809d1b2caa29a44cef41254c6720eb
Nicolas Nueca <nicolasnueca@nicolass-air.lan>
2023-05-02 23:29:07 -0700

    updated JS file

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-
+const gameBoard = document.querySelector('#gameBoard');
+const ctx = gameBoard.getContext('2d');
+const scoreText = document.querySelector('#scoreText');
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
