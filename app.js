ffa457deb6e0c0f96973360cf60f320747dad01c
Nicolas Nueca <nicolasnueca@nicolass-air.lan>
2023-05-02 23:49:58 -0700

    added event listeners and functions

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const scoreText = document.querySelector('#scoreText');
 const gameWidth = gameBoard.width;
 const gameHeight = gameBoard.height;
 const boardBackground = 'white';
-const snakeColor = 'green';
-const snakeBorder = 'black';
+const dragonColor = 'green';
+const dragonBorder = 'black';
 const foodColor = 'pink';
 const unitSize = 25;
 let running = false;
@@ -14,10 +14,28 @@ let ySpeed = 0;
 let foodX;
 let foodY;
 let score = 0;
-let snake = [
+let dragon = [
     {x:unitSize * 4, y:0},
     {x:unitSize * 3, y:0},
     {x:unitSize * 2, y:0},
     {x:unitSize, y:0},
     {x:0, y:0}
 ];
+
+window.addEventListener('keydown', changeDirection);
+resetBtn.addEventListener('click', resetGame);
+
+gameStart();
+
+function gameStart(){};
+function nextTick (){};
+function clearBoard (){};
+function createFood (){};
+function drawFood (){};
+function moveDragon (){};
+function drawDragon (){};
+function changeDirection (){};
+function checkGameOver (){};
+function displayGameOver (){};
+function resetGame (){};
+
