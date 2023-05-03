82cf3427da348f67ee91afea6b40f0db10719a0e
Nicolas Nueca <nicolasnueca@nicolass-air.lan>
2023-05-02 23:43:24 -0700

    added array and changed snake to dragon

--- app.js
+++ app.js
@@ -3,3 +3,21 @@ const ctx = gameBoard.getContext('2d');
 const scoreText = document.querySelector('#scoreText');
 const gameWidth = gameBoard.width;
 const gameHeight = gameBoard.height;
+const boardBackground = 'white';
+const snakeColor = 'green';
+const snakeBorder = 'black';
+const foodColor = 'pink';
+const unitSize = 25;
+let running = false;
+let xSpeed = unitSize
+let ySpeed = 0;
+let foodX;
+let foodY;
+let score = 0;
+let snake = [
+    {x:unitSize * 4, y:0},
+    {x:unitSize * 3, y:0},
+    {x:unitSize * 2, y:0},
+    {x:unitSize, y:0},
+    {x:0, y:0}
+];
